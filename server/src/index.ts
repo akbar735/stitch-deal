264b5f7c14bdcae6ba3aeb3d0d9bd267248ae536
ali_akbar7352 <mdaliakbar858@gmail.com>
2023-06-20 00:37:12 +0530

    added server

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app: express.Application = express();
+
+const PORT: number = Number(process.env.PORT) || 3001
+
+app.get("/", (req, res) => {
+        res.send("Hello World from TypeScript")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
+})
