f9ec0d082eae907013342a4c555e62e695cbfafc
ali_akbar7352 <mdaliakbar858@gmail.com>
2023-06-27 19:04:48 +0530

    deleted unused component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,10 @@ import PrimarySearchAppBar from './components/AppBar';
 import { Outlet } from 'react-router-dom';
 function App() {
   return (
-   
     <>
       <PrimarySearchAppBar />
       <Outlet />
     </>
-        
-
   );
 }
 
